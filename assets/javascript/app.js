66ff32e8029bed7c0b3211b39a241189ee2f7a5c
Ryan Goderis <rgoderis789@gmail.com>
2019-09-12 17:46:40 -0400

    created function that will display gifs array as buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,27 @@ var URL = "api.giphy.com/v1/gifs/search?q=";
 // limit
 // rating
 var gifs = []
+
+// function that displays gifs array content as buttons
+function renderButtons (){
+    // make sure button-container is empty
+    $("#button-container").empty()
+    // loop through gifs array
+    for(var i = 0; i < gifs.length; i++){
+        // create a button element saved to var b
+        var b = $("<button>");
+        // add class of gif to b
+        b.addClass("gif");
+        // set attribute of data-name to be what gif[i]
+        b.attr("data-name", gifs[i]);
+        // set button text to be gif[i]
+        b.text(gifs[i])
+        // add button to the button-container
+        $("#button-container").append(b)
+    }
+}
+
+
 $(document).ready(function() {
 
 // create buttons based on what the user enters in the search input
@@ -11,11 +32,11 @@ $(document).ready(function() {
 $("#submit").on("click", function(event){
     event.preventDefault();
     // grab the value that was entered in the input and save to a var
-    let gif = $("#search").val().trim()
-    console.log(gif)
+    let gif = $("#search").val().trim();
+    console.log(gif);
     // add gif to the gifs array
-    gifs.push(gif)
-})
+    gifs.push(gif);
+});
 
 
 
