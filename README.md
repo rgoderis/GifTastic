58bab1779eff6fac605d3e4b9e6e3a3d2b45dbde
Ryan Goderis <rgoderis789@gmail.com>
2019-10-26 16:10:43 -0400

    added text, link, and image to readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Displays gifs utilizing Giphy API
 
 User is able to create a button with the search bar which will access the gify API and display up to 10 gifs based of the search criterea when a button is clicked.
+
+Giftastic is a JavaScript application that utilizes API calls to the Giphy API.  The user starts with 10 Star Wars themed buttons that will make a call to the giphy api when clicked to display up to 10 of the top gifs based on the button's name.  When the user clicks on one of the gifs it will either play or stop playing depending on it's current state.  The user also has the option to create new buttons with the search bar to search for different gifs.
+
+![screenshot](./assets/images/giftastic)
+
+[GifTastic](https://rgoderis.github.io/GifTastic/)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,107 @@
-
-// q
-// limit
-// rating
-var topics = ["Jedi", "Stormtrooper", "Yoda", "Darth Vader", "Star Wars", "Han Solo", "Chewbacca", "Luke Skywalker", "Obi Wan Kenobi", "Anakin Skywalker"]
+const topics = ["Jedi", "Stormtrooper", "Yoda", "Darth Vader", "Star Wars", "Han Solo", "Chewbacca", "Luke Skywalker", "Obi Wan Kenobi", "Anakin Skywalker"];
 
 // function that displays gifs array content as buttons
-function renderButtons (){
+renderButtons=()=>{
     // make sure button-container is empty
     $("#button-container").empty()
     // loop through gifs array
-    for(var i = 0; i < topics.length; i++){
+    for(let i = 0; i < topics.length; i++){
         // create a button element saved to var b
-        var b = $("<button>");
+        let b = $("<button>");
         // add class of gif to b
         b.addClass("gifButton btn btn-dark");
         // set attribute of data-name to be what gif[i]
         b.attr("data-name", topics[i]);
         // set button text to be gif[i]
-        b.text(topics[i])
+        b.text(topics[i]);
         // add button to the button-container
-        $("#button-container").append(b)
+        $("#button-container").append(b);
     }
 }
 
 
-$(document).ready(function() {
-// call renderButton()
-renderButtons()
-
-// create buttons based on what the user enters in the search input
-// set click listener for submit button
-$("#submit").on("click", function(event){
-    event.preventDefault();
-    // grab the value that was entered in the input and save to a var
-    let gif = $("#search").val().trim();
-    // add gif to the gifs array
-    topics.push(gif);
+$(document).ready(()=> {
     // call renderButton()
-    renderButtons()
-    $("#search").val("")
-});
-
-// set click listener for created gif button to display gifs
-$(document).on("click", ".gifButton", function(){
-    // clear gif-container
-    $("#gif-container").empty();
-
-    // get attribute data-name of button clicked
-    var search = $(this).attr("data-name")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ search + "&api_key=w37zn0TRObt76z2J4oBU2NZm2HQUVtAf";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response)
-        // loop throught response and get 10 results
-        for(var i=0; i < 10; i++){
-            // find image URL for max-height 
-            var playGif = response.data[i].images.fixed_height.url
-            // find image URL for max-height still
-            var stillGif = response.data[i].images.fixed_height_still.url
-            // set rating to a var
-            var rating = response.data[i].rating
-            console.log(rating)
-            // create div to put img in
-            var div = $("<div>")
-            div.addClass("gifDiv")
-            // append rating to div
-            div.append("<p class='text-center'><strong>Rated: " + rating + "</strong></p>")
-            // create a img to display
-            var img = $("<img>")
-            // add class of gif
-            img.addClass("gif")
-            // add gif src of stillGif
-            img.attr("src", stillGif)
-            // add attribute of data-still set to stillGif src
-            img.attr("data-still", stillGif)
-            // add attribute of data-animate set to playGif src
-            img.attr("data-animate", playGif)
-            // add attribute of data-state for what the gif is currently doing
-            img.attr("data-state", "still")
-            // append img to div
-            div.append(img)
-            // append div to id gif-container
-            $("#gif-container").append(div)
-        }
-    })
-    
-});
-
-// set click listener for .gif to play and pause the gif
-$(document).on("click", ".gif", function(){
-    // set data-state to a var
-    var state = $(this).attr("data-state")
-    // check to see if state is still, if so animate the gif
-    if(state === "still"){
-        // change src to data-animate
-        $(this).attr("src", $(this).attr("data-animate"))
-        // change data-state to animate
-        $(this).attr("data-state", "animate")
-    } else {
-        // change src to data-still to stop playing
-        $(this).attr("src", $(this).attr("data-still"))
-        // change data-state to still
-        $(this).attr("data-state", "still")
-    }
-});
-
-$("#reset").on("click", function(){
-    $("#search-container").empty();
-    $("#gif-container").empty()
-    topics = ["Jedi", "Stormtrooper", "Yoda", "Darth Vader", "Star Wars", "Han Solo", "Chewbacca", "Luke Skywalker", "Obi Wan Kenobi", "Anakin Skywalker"];
     renderButtons();
-})
+    // create buttons based on what the user enters in the search input
+    // set click listener for submit button
+    $("#submit").on("click", event=>{
+        event.preventDefault();
+        // grab the value that was entered in the input and save to a var
+        let gif = $("#search").val().trim();
+        // add gif to the gifs array
+        topics.push(gif);
+        // call renderButton()
+        renderButtons();
+        $("#search").val("");
+    });
 
+    // set click listener for created gif button to display gifs
+    $(document).on("click", ".gifButton", function(){
+        // clear gif-container
+        $("#gif-container").empty();
+        // get attribute data-name of button clicked
+        let search = $(this).attr("data-name")
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q="+ search + "&api_key=w37zn0TRObt76z2J4oBU2NZm2HQUVtAf";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(response=>{
+            // loop throught response and get 10 results
+            for(let i=0; i < 10; i++){
+                // find image URL for max-height 
+                let playGif = response.data[i].images.fixed_height.url;
+                // find image URL for max-height still
+                let stillGif = response.data[i].images.fixed_height_still.url;
+                // set rating to a var
+                let rating = response.data[i].rating;
+                // create div to put img in
+                let div = $("<div>");
+                div.addClass("gifDiv");
+                // append rating to div
+                div.append("<p class='text-center'><strong>Rated: " + rating + "</strong></p>");
+                // create a img to display
+                let img = $("<img>");
+                // add class of gif
+                img.addClass("gif");
+                // add gif src of stillGif
+                img.attr("src", stillGif);
+                // add attribute of data-still set to stillGif src
+                img.attr("data-still", stillGif);
+                // add attribute of data-animate set to playGif src
+                img.attr("data-animate", playGif);
+                // add attribute of data-state for what the gif is currently doing
+                img.attr("data-state", "still");
+                // append img to div
+                div.append(img);
+                // append div to id gif-container
+                $("#gif-container").append(div);
+            }
+        });
+    });
+
+    // set click listener for .gif to play and pause the gif
+    $(document).on("click", ".gif", function(){
+        // set data-state to a var
+        let state = $(this).attr("data-state");
+        // check to see if state is still, if so animate the gif
+        if(state === "still"){
+            // change src to data-animate
+            $(this).attr("src", $(this).attr("data-animate"));
+            // change data-state to animate
+            $(this).attr("data-state", "animate");
+        } else {
+            // change src to data-still to stop playing
+            $(this).attr("src", $(this).attr("data-still"));
+            // change data-state to still
+            $(this).attr("data-state", "still");
+        }
+    });
+
+    $("#reset").on("click", ()=>{
+        $("#search-container").empty();
+        $("#gif-container").empty();
+        topics = ["Jedi", "Stormtrooper", "Yoda", "Darth Vader", "Star Wars", "Han Solo", "Chewbacca", "Luke Skywalker", "Obi Wan Kenobi", "Anakin Skywalker"];
+        renderButtons();
+    });
 });
